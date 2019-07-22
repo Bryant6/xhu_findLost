cd554bd77f17bb03d47fb4ae744a86cf16765f45
chennumberone <527455165@qq.com>
2019-07-22 22:17:06 +0800

    实现动态信息分类获取和底部刷新和缓存优化

--- pages/main/main.js
+++ pages/main/main.js
@@ -1,16 +1,41 @@
 // pages/main/main.js
 const app = getApp();
-const url=app.globalData.url;
+const url = app.globalData.url;
+
+//封装request为Promise
+function createPromise(dataUrl, page, kind) {
+  return new Promise(function(resolve, reject) {
+    wx.request({
+      url: url + dataUrl,
+      data: {
+        page: page,
+        kind: kind
+      },
+      success(res) {
+        resolve(res);
+      }
+    })
+  })
+}
+
+function Toast() {
+  wx.showToast({
+    title: '亲，我已经到底了',
+    icon: 'none',
+    duration: 2000
+  })
+}
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    findOwner: null,
     CustomBar: app.globalData.CustomBar,
-    selected: true,//true 为寻找失物   false认领失物
-    cardCur: 0,
+    dataList: null, //前台渲染数据
+    selected: true, //true 为寻找失物   false认领失物
+    bigKind: '全部', //大类
+    cardCur: 0, //轮番图
     swiperList: [{
       id: 0,
       type: 'image',
@@ -46,24 +71,65 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    wx.request({
-      url: url+'findOwner/findAll',
-      success: data => {
-        console.log(data.data);
-        this.setData({
-          findOwner: data.data
-        })
-      },
-      fail() {
-        console.log(error);
+    let that = this;
+    Promise.all([
+      //获取全部找失物数据
+      createPromise("findGoodsSort", 0, '全部'),
+      //获取大类找失主数据
+      createPromise("findOwnerSort", 0, '全部')
+    ]).then(function(res) {
+      let [data1, data2] = res;
+      that.setData({
+        dataList: data1.data
+      })
+      let json_obj = {
+        "findOwnerSort": data2.data,
+        "findGoodsSort": data1.data,
+        "findOwnerSortPage": 0,
+        "findGoodsSortPage": 0
       }
+      wx.setStorage({
+        key: '全部',
+        data: json_obj,
+      })
+      console.log("全部--存入缓存成功")
     })
+
   },
-  selected_btn1:function(){
-    this.setData({selected:true})
+  //切换寻找失物
+  selected_btn1: function() {
+    if (!this.data.selected) {
+      let kind = this.data.bigKind;
+      console.log(kind + "切换到寻找失物");
+      //按照大类读取缓存
+      try {
+        let value = wx.getStorageSync(kind)
+        this.setData({
+          selected: true,
+          dataList: value.findGoodsSort
+        })
+      } catch (e) {
+        console.log("读取缓存失败");
+      }
+    }
   },
-  selected_btn2: function () {
-    this.setData({selected:false})
+  //切换认领失物
+  selected_btn2: function() {
+    if (this.data.selected) {
+      let kind = this.data.bigKind;
+      console.log(kind + "切换到认领失物");
+      //按照大类读取缓存
+      try {
+        let value = wx.getStorageSync(kind)
+        console.log(value);
+        this.setData({
+          selected: false,
+          dataList: value.findOwnerSort
+        })
+      } catch (e) {
+        console.log("读取缓存失败");
+      }
+    }
   },
   cardSwiper(e) {
     this.setData({
@@ -80,4 +146,156 @@ Page({
       modalName: null
     })
   },
+  //选择大类
+  bigKind_change: function(e) {
+    var that = this;
+    var bigKind = e.currentTarget.dataset.bigkind;
+    console.log("大类：" + bigKind);
+    this.setData({
+      bigKind: bigKind,
+      modalName: null
+    })
+
+    //判断是否有此key(大类)，以便缓存
+    try {
+      const res = wx.getStorageInfoSync()
+      let haveKey = false; //是否拥有key(大类)
+      for (let i = 0; i < res.keys.length; i++) {
+        if (res.keys[i] == bigKind) {
+          haveKey = true;
+          break;
+        }
+      }
+      //有此大类 读取缓存直接渲染
+      if (haveKey) {
+        console.log("缓存中有此大类")
+        try {
+          let list = wx.getStorageSync(bigKind);
+          if (that.data.selected) {
+            console.log(list.findGoodsSort)
+            that.setData({
+              dataList: list.findGoodsSort
+            })
+          } else {
+            console.log(list.findOwnerSort)
+            that.setData({
+              dataList: list.findOwnerSort
+            })
+          }
+        } catch (e) {
+          console.log(e);
+        }
+
+      } else {
+        console.log("缓存中没有此大类")
+        //没有此大类  请求数据 并且缓存数据
+        let json_obj = {
+          "findOwnerSort": "",
+          "findGoodsSort": "",
+          "findOwnerSortPage": 0,
+          "findGoodsSortPage": 0
+        };
+
+        Promise.all([
+          //获取大类找失物数据
+          createPromise("findGoodsSort", 0, that.data.bigKind),
+          //获取大类找失主数据
+          createPromise("findOwnerSort", 0, that.data.bigKind)
+        ]).then(function(res) {
+          let [data1, data2] = res;
+          if (that.data.selected) {
+            that.setData({
+              dataList: data1.data
+            })
+          } else {
+            that.setData({
+              dataList: data2.data
+            })
+          }
+
+          json_obj.findGoodsSort = data1.data;
+          json_obj.findOwnerSort = data2.data;
+          //存入缓存
+          wx.setStorage({
+            key: that.data.bigKind,
+            data: json_obj,
+          })
+          console.log("存入缓存成功")
+        })
+
+      }
+    } catch (e) {
+      console.log(e)
+    }
+  },
+
+  //触底刷新
+  scrolltoBottom: function() {
+    var that = this;
+    console.log("到达底部");
+    let kind = this.data.bigKind;
+    console.log(kind)
+    try {
+      let value = wx.getStorageSync(kind);
+      if (this.data.selected) {
+        wx.request({
+          url: url + 'findGoodsSort',
+          data: {
+            page: value.findGoodsSortPage,
+            kind: kind
+          },
+          success: function(res) {
+            if (res.data.length == 0) {
+              Toast();
+            }
+            console.log("更新数据");
+            let newData = value.findGoodsSort.concat(res.data);
+            console.log(newData);
+            that.setData({
+              dataList: newData
+            })
+            //更新缓存
+            let page = value.findGoodsSortPage + 6;
+            value.findGoodsSort = newData;
+            value.findGoodsSortPage = page;
+            wx.setStorage({
+              key: kind,
+              data: value
+            })
+            console.log("更新缓存")
+          }
+        })
+      } else {
+        wx.request({
+          url: url + 'findOwnerSort',
+          data: {
+            page: value.findOwnerSortPage,
+            kind: kind
+          },
+          success: function(res) {
+            if (res.data.length == 0) {
+              Toast();
+            }
+            console.log("更新数据");
+            let newData = value.findOwnerSort.concat(res.data);
+            console.log(newData);
+            that.setData({
+              dataList: newData
+            })
+            //更新缓存
+            let page = value.findOwnerSortPage + 6;
+            value.findOwnerSort = newData;
+            value.findOwnerSortPage = page;
+            wx.setStorage({
+              key: kind,
+              data: value
+            })
+            console.log("更新缓存")
+          }
+        })
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
 })
